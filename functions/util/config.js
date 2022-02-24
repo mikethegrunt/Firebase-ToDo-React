00922952a56c6cbd812f2b5aa86d96352de96fea
Michael Williamson <mikethegrunt@gmail.com>
2022-02-24 12:12:31 -0600

    Delete config.js

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyBtigaAdQ51Y54G2mU4oiSkNeZy4jPdlEk",
-  authDomain: "todoapp-f8493.firebaseapp.com",
-  databaseURL: "https://todoapp-f8493-default-rtdb.firebaseio.com",
-  projectId: "todoapp-f8493",
-  storageBucket: "todoapp-f8493.appspot.com",
-  messagingSenderId: "767404629695",
-  appId: "1:767404629695:web:2ad8a8a7d4b613625af00a"
-};
